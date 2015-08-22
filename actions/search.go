734e769293ea8efe5eca4c2ff3f2cea9b3a41289
Matthias Thiel <mail@matthiasthiel.com>
2015-08-22 23:43:10 +0200

    write short ids for search results

--- actions/search.go
+++ actions/search.go
@@ -51,11 +51,15 @@ func Search(c *cli.Context) {
 	query.Skip((page - 1) * perPage).Limit(perPage).All(&result)
 
 	if total > 0 {
+		data.ClearCache()
+		defer data.FlushCache()
+
 		for index, object := range result {
 			fmt.Printf("(%s) %s \"%s\" %s\n", colors.ShortId(index+1),
 				colors.ObjectId(object.Id.Hex()),
 				object.Title,
 				colors.Faint(fmt.Sprintf("[%.2f]", object.Score)))
+			data.SetCache(strconv.Itoa(index+1), object.Id.Hex())
 		}
 		fmt.Printf("Page %s of %s\n", colors.Bold(strconv.Itoa(page)), colors.Bold(strconv.Itoa(int(total/perPage)+1)))
 	}
